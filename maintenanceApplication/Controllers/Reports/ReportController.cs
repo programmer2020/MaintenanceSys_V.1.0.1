ad74b57ca1cb2c4f46a08f7ff3b46dcc335b192d
unknown <mohamed.programmer.network@gmail.com>
2020-07-13 08:32:33 +0400

    'update_maintenance'

--- maintenanceApplication/Controllers/Reports/ReportController.cs
+++ maintenanceApplication/Controllers/Reports/ReportController.cs
@@ -71,5 +71,51 @@ namespace maintenanceApplication.Controllers.Reports
 
         }
 
+
+
+
+
+
+        [HttpGet]
+        public ActionResult TechnicalReport()
+        {
+
+            return View("TechnicalReport", new MaintenanceModel());
+        }
+
+        [HttpPost]
+        public ActionResult TechnicalReport(int id)
+        {
+            try
+            {
+                if (id != 0)
+                {
+                    //var maintenanceSatus = _context.status.Where(x => x.StatusName == "Delivered").FirstOrDefault();
+                    //int statusId = maintenanceSatus.Id;
+                    var maintenance_requests = _context.maintenance.Where(x => x.Id == id).Include(x => x.priority).Include(x => x.status).Include(x => x.CustomerCity).FirstOrDefault();
+
+                    if (maintenance_requests != null)
+                    {
+                        return View("TechnicalReport", maintenance_requests);
+                    }
+                    else
+                    {
+                        return View("TechnicalReport", new MaintenanceModel());
+                    }
+                }
+                else
+                {
+                    return View("TechnicalReport", new MaintenanceModel());
+                }
+
+            }
+            catch
+            {
+                return View("TechnicalReport", new MaintenanceModel());
+            }
+
+        }
+
+
     }
 }
